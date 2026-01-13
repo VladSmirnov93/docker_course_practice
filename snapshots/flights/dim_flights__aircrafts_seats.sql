b493377eca2ae3e649b9204f3027df14680ffee3
smirnov_vlad <vladsmirnov93@yandex.ru>
2026-01-13 15:30:50 +0300

    5.5

---
+++ snapshots/flights/dim_flights__aircrafts_seats.sql
@@ -0,0 +1,19 @@
+{% snapshot dim_flights__aircrafts_seats %}
+
+{{
+  config(
+    target_schema='snapshot',
+    unique_key="aircraft_code || '-' || seat_no",
+    strategy='check',
+    check_cols=['fare_conditions'],
+    dbt_valid_to_current="'9999-01-01'::date"
+  )
+}}
+
+select 
+    aircraft_code, 
+    seat_no, 
+    fare_conditions
+from {{ ref('stg_flights__seats') }}
+
+{% endsnapshot %}
